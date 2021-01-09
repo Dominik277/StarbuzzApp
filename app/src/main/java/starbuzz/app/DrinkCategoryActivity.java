f92f086adcb3d5c4b4956e927af1e6ef97a57714
Dominik277 <dominikdoric277@gmail.com>
2021-01-09 20:02:32 +0100

    Dodani ListView-ovi u svaki activity i implementirani onClickListener-i.

--- app/src/main/java/starbuzz/app/DrinkCategoryActivity.java
+++ app/src/main/java/starbuzz/app/DrinkCategoryActivity.java
@@ -2,13 +2,41 @@ package starbuzz.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+
+    ListView listViewCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
+        listViewCategory = findViewById(R.id.lisViewCategory);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Latte");
+        arrayList.add("Cappuccino");
+        arrayList.add("Filter");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this
+                , android.R.layout.simple_list_item_1,arrayList);
+        listViewCategory.setAdapter(arrayAdapter);
+
+        listViewCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/starbuzz/app/TopLevelActivity.java
+++ app/src/main/java/starbuzz/app/TopLevelActivity.java
@@ -2,13 +2,41 @@ package starbuzz.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+
+        listView = findViewById(R.id.listView);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Drinks");
+        arrayList.add("Food");
+        arrayList.add("Stores");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this
+                ,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this,DrinkCategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
