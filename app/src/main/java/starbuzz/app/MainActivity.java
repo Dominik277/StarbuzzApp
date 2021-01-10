2082fe3e1579a82f57f3d6f18da5a25234976275
Dominik277 <dominikdoric277@gmail.com>
2021-01-10 18:50:25 +0100

    Dodan action provider.

--- app/src/main/java/starbuzz/app/MainActivity.java
+++ app/src/main/java/starbuzz/app/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ActionProvider;
+import androidx.core.view.MenuItemCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent(String text){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
