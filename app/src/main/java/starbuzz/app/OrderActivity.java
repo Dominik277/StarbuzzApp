dab5b3cdd9247dd472f2d7872fc2b4b38f3593c6
Dominik277 <dominikdoric277@gmail.com>
2021-01-10 17:06:38 +0100

    Dodano par stvari.

--- app/src/main/java/starbuzz/app/OrderActivity.java
+++ app/src/main/java/starbuzz/app/OrderActivity.java
@@ -1,17 +1,14 @@
 package starbuzz.app;
 
-import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_order);
     }
 }
