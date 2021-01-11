5ac8171d6cd598caeb256c08d71554ab81808a1f
Dominik277 <dominikdoric277@gmail.com>
2021-01-11 20:05:33 +0100

    Dodano podosta toga.

--- app/src/main/java/starbuzz/app/MainActivity.java
+++ app/src/main/java/starbuzz/app/MainActivity.java
@@ -2,52 +2,23 @@ package starbuzz.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ShareActionProvider;
+import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ActionProvider;
-import androidx.core.view.MenuItemCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        MenuItem menuItem = menu.findItem(R.id.action_share);
-        shareActionProvider = MenuItemCompat.getActionProvider(menuItem);
-        setShareActionIntent("Want to join me for pizza?");
-        return super.onCreateOptionsMenu(menu);
+    public void onShowDetails(View view){
+        Intent intent = new Intent(MainActivity.this,DetailActivity.class);
+        startActivity(intent);
     }
 
-    private void setShareActionIntent(String text){
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT,text);
-        shareActionProvider.setShareIntent(intent);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_create_order:
-                Intent intent = new Intent(this,OrderActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
---
+++ app/src/main/java/starbuzz/app/Workout.java
@@ -0,0 +1,4 @@
+package starbuzz.app;
+
+public class Workout {
+}
