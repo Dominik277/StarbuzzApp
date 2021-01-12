f311d5fcf45e70ec8b7196a429aeba42568651f4
Dominik277 <dominikdoric277@gmail.com>
2021-01-12 10:57:27 +0100

    Azuriran kod za WorkoutListFragment.

--- app/src/main/java/starbuzz/app/WorkoutListFragment.java
+++ app/src/main/java/starbuzz/app/WorkoutListFragment.java
@@ -1,14 +1,24 @@
 package starbuzz.app;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutListFragment extends ListFragment{
 
+
+    static interface Listener{
+        void itemClicked(long id);
+    };
+
+    private Listener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,5 +31,18 @@ public class WorkoutListFragment extends ListFragment{
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(inflater.getContext(), android.R.layout.simple_list_item_1,names);
         setListAdapter(adapter);
         return super.onCreateView(inflater,container,savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.listener = (Listener) context;
+    }
+
+    @Override
+    public void onListItemClick(@NonNull ListView l, @NonNull View v, int position, long id) {
+        if (listener != null){
+            listener.itemClicked(id);
+        }
     }
 }
