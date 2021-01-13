4241e4b55d1835010cfe6651189b1e566d2cc515
Dominik277 <dominikdoric277@gmail.com>
2021-01-14 00:06:00 +0100

    Implemenitrana klasa StarbuzzDatabaseHelper koja ce biti zaduzena za operacije vezane za bazu podataka.

--- app/src/main/java/starbuzz/app/Drink.java
+++ app/src/main/java/starbuzz/app/Drink.java
@@ -2,7 +2,10 @@ package starbuzz.app;
 
 public class Drink {
 
-    private String name; private String description; private int imageResourceId;
+    private String name;
+    private String description;
+    private int imageResourceId;
+
     //drinks is an array of Drinks
     public static final Drink[] drinks = {
             new Drink("Latte", "A couple of espresso shots with steamed milk", R.drawable.latte),
---
+++ app/src/main/java/starbuzz/app/StarbuzzDatabaseHelper.java
@@ -0,0 +1,25 @@
+package starbuzz.app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
+
+
+    public StarbuzzDatabaseHelper(@Nullable Context context) {
+        super(Context context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
